273ee07a98ad291acea52e5fa778aac07d2ac582
cortesem <michael.cortese@ryerson.ca>
2023-06-25 07:11:26 -0400

    update dashboard code to scan for open node ports

--- pkg/dashboard/dashboard.go
+++ pkg/dashboard/dashboard.go
@@ -1,25 +1,47 @@
 package dashboard
 
 import (
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"io"
 	"log"
+	"net/http"
 	"os"
+	clustermonitor "simple-cluster-dashboard/pkg/cluster_monitor"
+	"strconv"
 	"time"
 )
 
 type HTMLPage struct {
-	Title string
-	HTML  *template.Template
-	CSS   template.CSS
+	Title   string
+	HTML    *template.Template
+	CSS     template.CSS
+	Cluster clustermonitor.Cluster
 	globalStatus
 	nodePortData
+	nodeData
+}
+
+type htmlPageInfo struct {
+	Status     string
+	StatusText string
+	StatusOK   bool
 }
 
 type globalStatus struct {
 	GlobalStatus     string
 	GlobalStatusText string
+}
+
+type nodeData struct {
+	Nodes []Node
+}
+
+type Node struct {
+	Name string
+	Role string
+	htmlPageInfo
 }
 
 type nodePortData struct {
@@ -28,12 +50,10 @@ type nodePortData struct {
 }
 
 type NodePort struct {
-	Name       string
-	Endpoint   string
-	Port       string
-	Status     string
-	StatusText string
-	StatusOK   bool
+	Name     string
+	Endpoint string
+	Port     string
+	htmlPageInfo
 }
 
 func (h *HTMLPage) New(title string) {
@@ -48,11 +68,14 @@ func (h *HTMLPage) New(title string) {
 	h.Title = title
 	h.CSS = template.CSS(css)
 
+	h.Cluster = *clustermonitor.NewOutClusterClient()
+	h.Cluster.Update()
+
 	h.Update()
 }
 
 func (h *HTMLPage) Update() {
-	h.nodePortData.update()
+	h.nodePortData.update(&h.Cluster)
 	h.globalStatus.update(&h.nodePortData)
 }
 
@@ -77,30 +100,61 @@ func (g *globalStatus) update(n *nodePortData) {
 	}
 }
 
-func (n *nodePortData) update() {
-	n.fetchNodePorts()
+func (n *nodePortData) update(c *clustermonitor.Cluster) {
+	n.fetchNodePorts(c)
 	n.timeStamp()
 }
 
-func (n *nodePortData) fetchNodePorts() {
-	n.NodePorts = []NodePort{
-		{
-			Name:       "Tekton",
-			Endpoint:   "http://k3s.local",
-			Port:       "31083",
-			Status:     "failed",
-			StatusText: "Disrupted",
-			StatusOK:   false,
-		},
-		{
-			Name:       "Harbor",
-			Endpoint:   "https://k3s.local",
-			Port:       "30003",
-			Status:     "success",
-			StatusText: "Operational",
-			StatusOK:   true,
-		},
+func (n *nodePortData) fetchNodePorts(c *clustermonitor.Cluster) {
+	var nodePorts []NodePort
+
+	// skip tls
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
+	client := &http.Client{Transport: tr}
+	// Check for the first responsive port we can find and use that else mark as unavailable.
+	for _, np := range c.NodePorts {
+		// Find a responsive port (one that returns a 200)
+		for _, port := range np.Ports {
+			var prefix string
+			if port.PortName == "https" {
+				prefix = "https://"
+			} else {
+				prefix = "http://"
+			}
+			endpoint := fmt.Sprintf("%sk3s.local:%s", prefix, strconv.Itoa(int(port.Port)))
+			resp, err := client.Get(endpoint)
+			if err != nil {
+				continue
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode == 200 {
+				nodePorts = append(nodePorts, NodePort{
+					Name:     np.ServiceName,
+					Endpoint: fmt.Sprintf("%sk3s.local", prefix),
+					Port:     strconv.Itoa(int(port.Port)),
+					htmlPageInfo: htmlPageInfo{
+						Status:     "success",
+						StatusText: "Operational",
+						StatusOK:   true,
+					},
+				})
+				break
+			}
+		}
+	}
+
+	n.NodePorts = nodePorts
+
+	// Status:     "failed",
+	// StatusText: "Disrupted",
+	// StatusOK:   false,
+
+	// Status:     "success",
+	// StatusText: "Operational",
+	// StatusOK:   true,
+
 }
 
 func (n *nodePortData) timeStamp() {
